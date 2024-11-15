75d0a520bacb98a6127b3bb47aff27e047a228ab
Степан <stepkamakarov1999@mail.ru>
2024-11-15 18:06:06 +0300

    Update deploy_star_burger.sh

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -10,4 +10,13 @@ npm ci --dev
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
 systemctl reload nginx
+curl -X POST "https://api.rollbar.com/api/1/deploy/" \
+     -H "Content-Type: application/json" \
+     -H "X-Rollbar-Access-Token: a0b492b873cb4cd482ac15880d4c8c4c" \
+     -d "{
+           \"environment\": \"production\",
+           \"revision\": \"$(git rev-parse HEAD)\",
+           \"local_username\": \"$(whoami)\",
+           \"comment\": \"Deployment completed\"
+         }"
 echo "Деплой завершен успешен"
